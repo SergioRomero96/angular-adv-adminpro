c3606f40a04b75b98639b43fb7dfcbd48379a3f6
SergioRomero96 <sergio.nicolas777@gmail.com>
2020-11-14 21:03:07 -0300

    Fin sección 6

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class Grafica1Component implements OnInit {
+  labels1: string[] = ['Tacos', 'Pan', 'Refresco'];
+  data1 = [350, 450, 100];
 
   constructor() { }
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
+import { PagesRoutingModule } from './pages-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
-import { PagesRoutingModule } from './pages-routing.module';
+
 
 
 
@@ -17,7 +22,7 @@ import { PagesRoutingModule } from './pages-routing.module';
     Grafica1Component,
     PagesComponent
   ],
-  exports:[
+  exports: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
@@ -27,7 +32,9 @@ import { PagesRoutingModule } from './pages-routing.module';
     CommonModule,
     PagesRoutingModule,
     RouterModule,
-    SharedModule
+    FormsModule,
+    SharedModule,
+    ComponentsModule, 
   ]
 })
 export class PagesModule { }
